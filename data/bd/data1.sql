b9e81c347109cf8d7f65dfa6e5db992474adba9d
Alisson Chiquitto <chiquitto@gmail.com>
2016-06-23 11:45:34 -0300

    Correções para mysql 5.7

--- data/bd/data1.sql
+++ data/bd/data1.sql
@@ -3,19 +3,19 @@
 DROP TABLE IF EXISTS `tbdtb`;
 
 CREATE TABLE `tbdtb` (
-    UF int(11) NOT NULL,
+    UF BIGINT NOT NULL,
     Nome_UF varchar(255) NOT NULL,
-    Mesorregiao_Geografica int(11) NOT NULL,
+    Mesorregiao_Geografica BIGINT NOT NULL,
     Nome_Mesorregiao varchar(255) NOT NULL,
-    Microrregiao_Geografica int(11) NOT NULL,
+    Microrregiao_Geografica BIGINT NOT NULL,
     Nome_Microrregiao varchar(255) NOT NULL,
-    Municipio int(11) NOT NULL,
-    Cod_Municipio_Completo int(11) NOT NULL,
+    Municipio BIGINT NOT NULL,
+    Cod_Municipio_Completo BIGINT Unsigned NOT NULL,
     Nome_Municipio varchar(255) NOT NULL,
-    Distrito int(11) NOT NULL,
-    Cod_Distrito_Completo int(11) NOT NULL,
+    Distrito BIGINT NOT NULL,
+    Cod_Distrito_Completo BIGINT Unsigned NOT NULL,
     Nome_Distrito varchar(255) NOT NULL,
-    Subdistrito int(11) NOT NULL,
-    Cod_SubDistrito_Completo int(11) NOT NULL,
+    Subdistrito BIGINT NOT NULL,
+    Cod_SubDistrito_Completo BIGINT Unsigned NOT NULL,
     Nome_Subdistrito varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
